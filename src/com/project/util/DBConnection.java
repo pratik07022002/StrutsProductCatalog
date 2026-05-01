9445ac236cd084605270f2510036877386251f38
Subradeep <subradeepdas24@gmail.com>
2026-05-01 20:20:11 +0530

    Bugs Fixed added DB connection succesfully

--- src/com/project/util/DBConnection.java
+++ src/com/project/util/DBConnection.java
@@ -2,30 +2,34 @@ package com.project.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/productdb?useSSL=false&serverTimezone=UTC";
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/productdb?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "your_password_here";
+    private static final String PASSWORD = "240102";
 
     public static Connection getConnection() {
-        Connection con = null;
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            con = DriverManager.getConnection(URL, USER, PASSWORD);
+            Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
 
             if (con != null) {
                 System.out.println("DB CONNECTED SUCCESSFULLY");
             }
 
-        } catch (Exception e) {
+            return con;
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found");
+            e.printStackTrace();
+        } catch (SQLException e) {
             System.out.println("DB CONNECTION FAILED");
             e.printStackTrace();
         }
 
-        return con;
+        return null;
     }
 }
